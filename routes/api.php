e28c269c5174a2be68a24bd7aada889d85fd5beb
NorlanCoder <fabien.amoussou20@gmail.com>
2024-06-20 00:28:54 +0100

    Category Endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +22,17 @@ Route::post('forget', [AuthController::class, 'forget'])->name('forget');
 Route::post('validate_token', [AuthController::class, 'validate_token'])->name('validate_token');
 Route::post('reset', [AuthController::class, 'reset'])->name('reset');
 
+// Category end-point
+Route::get('list/category', [CategoryController::class,'listcategory'])->name('listcategory');
+Route::get('list/category/property', [CategoryController::class,'listcategoryproperty'])->name('listcategoryproperty');
+
 Route::middleware(['auth:sanctum'])->group(function () {
-    
+
     Route::middleware(['visitor'])->group(function () {
-    
+
     });
 
     Route::middleware(['announcer'])->group(function () {
-    
+
     });
 });
